696ce28c330ba7380ec283b2665630467e8f68b3
Mert <mertefeisawesome@gmail.com>
2021-03-22 02:47:49 +0300

    17. Gün - Ödev 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CarComponent } from './components/car/car.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BrandComponent } from './components/brand/brand.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    CarComponent,
+    NaviComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule,
